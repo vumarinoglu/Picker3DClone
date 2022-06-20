cfb4cd2f2fbbe21358c8d48c253065c3cbb84f05
Atakan <s.atakancankaya@gmail.com>
2022-06-20 13:54:06 +0300

    Collectible Object Prefab and Script

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private bool collected = false;
+
+    private void OnEnable()
+    {
+        GameManager.OnEndOfChapter += PushObject;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.OnEndOfChapter -= PushObject;
+    }
+
+    public void PushObject()
+    {
+        if (!collected) return;
+        var rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.forward * 10f, ForceMode.Impulse);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            collected = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            collected = false;
+        }
+    }
+
+}
