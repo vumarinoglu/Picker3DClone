a984095fce5907159c8fd59988ee4b20bcd01234
Atakan <s.atakancankaya@gmail.com>
2022-06-20 13:55:56 +0300

    Level Parts - ChapterEnd

---
+++ Assets/Scripts/LevelParts.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelParts : MonoBehaviour
+{
+    public List<GameObject> chapters;
+    public List<GameObject> chapterEndings;
+    public List<GameObject> waypoints;
+}
