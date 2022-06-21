d7f8bd99fdb817f5c0092f80f1e0f78a4572dfac
Atakan <s.atakancankaya@gmail.com>
2022-06-22 01:48:34 +0300

    MouseInput Movement

---
+++ Assets/Scripts/MouseInput.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInput : MonoBehaviour
+{
+    private float lastMousePos;
+    private float _moveXFactor;
+    public float moveXFactor => _moveXFactor;
+
+    private void Update()
+    {
+        TakeInput();
+    }
+
+    public void TakeInput()
+    {
+        switch (GameManager.Instance.GetGameState())
+        {
+            case GameManager.GameState.START:
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GameManager.Instance.ChangeGameState(GameManager.GameState.GAMEPLAY);
+                }
+
+                break;
+            case GameManager.GameState.GAMEPLAY:
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    lastMousePos = Input.mousePosition.x;
+                }
+                else if (Input.GetMouseButton(0))
+                {
+                    _moveXFactor = Input.mousePosition.x - lastMousePos;
+                    lastMousePos = Input.mousePosition.x;
+                }
+                else if (Input.GetMouseButtonUp(0))
+                {
+                    _moveXFactor = 0;
+                }
+
+                break;
+            case GameManager.GameState.GAMEOVER:
+
+
+                break;
+            default:
+                break;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public abstract class Movement : MonoBehaviour
+{
+    [SerializeField] 
+    private float speed;
+    
+    protected bool isActive;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 direction)
+    {
+        if (!isActive) return;
+
+        Vector3 dirNormalized = direction.normalized;
+        dirNormalized.z = 1f;
+        dirNormalized.x *= 1.25f;
+        Vector3 offset =  dirNormalized * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + offset);
+    }
+}
