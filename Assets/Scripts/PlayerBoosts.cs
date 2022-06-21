13c6aecc20ab5977849517c3cb112108a0ac9720
Atakan <s.atakancankaya@gmail.com>
2022-06-22 01:47:32 +0300

    Player Related Scripts
    Movement and boost

---
+++ Assets/Scripts/PlayerBoosts.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBoosts : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject wings;
+
+    public static Action<BoosterType> OnBoosterTaken;
+    public static Action OnWingsClosed;
+
+    private void OnEnable()
+    {
+        GameManager.OnEndOfChapter += CloseWings;
+        GameManager.OnChapterWon += CloseWings;
+        GameManager.OnChapterLost += CloseWings;
+        OnBoosterTaken += BoosterTaken;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.OnEndOfChapter -= CloseWings;
+        GameManager.OnChapterWon -= CloseWings;
+        GameManager.OnChapterLost -= CloseWings;
+        OnBoosterTaken -= BoosterTaken;
+    }
+
+    public void BoosterTaken(BoosterType boosterType)
+    {
+        switch (boosterType)
+        {
+            case BoosterType.WINGS:
+                wings.SetActive(true);
+                break;
+            case BoosterType.SIZEUP:
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void CloseWings(int chapter)
+    {
+        wings.SetActive(false);
+        OnWingsClosed?.Invoke();
+    }
+
+    public void CloseWings()
+    {
+        wings.SetActive(false);
+        OnWingsClosed?.Invoke();
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : Movement
+{
+    [SerializeField]
+    private MouseInput mouseInput;
+
+    [SerializeField]
+    private float minX, maxX;
+
+    private void OnEnable()
+    {
+        GameManager.OnEndOfChapter += StopMovement;
+        GameManager.OnChapterWon += StartMovement;
+        GameManager.OnChapterLost += StopMovement;
+        GameManager.OnGamePlayStarted += StartMovement;
+        GameManager.OnEndLevel += StopMovement;
+        GameManager.OnNextLevel += StartMovement;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.OnEndOfChapter -= StopMovement;
+        GameManager.OnChapterWon -= StartMovement;
+        GameManager.OnChapterLost -= StopMovement;
+        GameManager.OnGamePlayStarted -= StartMovement;
+        GameManager.OnEndLevel -= StopMovement;
+        GameManager.OnNextLevel += StartMovement;
+    }
+
+    private void Update()
+    {
+        switch (GameManager.Instance.GetGameState())
+        {
+            case GameManager.GameState.START:
+
+                isActive = false;
+
+                break;
+            case GameManager.GameState.GAMEPLAY:
+
+                transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX),
+                    transform.position.y, transform.position.z);
+
+                break;
+            case GameManager.GameState.GAMEOVER:
+
+                isActive = false;
+
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if(!isActive) return;
+
+        Move(new Vector3(mouseInput.moveXFactor, 0f, 1f));
+    }
+
+    public void StopMovement()
+    {
+        isActive = false;
+    }
+
+    public void StartMovement(int chapter)
+    {
+        isActive = true;
+    }
+
+    public void StartMovement()
+    {
+        isActive = true;
+    }
+}
